81653baa42e59946e1b06e1ab38cdec2e343ad4d
Riyadh Arridha <97424850+riyadh1987@users.noreply.github.com>
2022-02-03 11:18:07 +0900

    Add files via upload

---
+++ src/components/molecules/TableHeader/TableHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import CurrencyTitle from '../../atoms/currencyTitle/CurrencyTitle';
+import "./TableHeader.css"
+
+const TableHeader = ({text1, text2, text3, text4}) => {
+    return (
+        <div class="list">
+            <CurrencyTitle text={text1}/>
+            <CurrencyTitle text={text2}/>
+            <CurrencyTitle text={text3}/>
+            <CurrencyTitle text={text4}/>
+        </div>
+    )
+}
+
+export default TableHeader;
