c7c486c0196926394c3fa1382079e627e5149595
Riyadh Arridha <97424850+riyadh1987@users.noreply.github.com>
2022-03-01 22:37:44 +0900

    Add files via upload

--- src/screen/CurrencyScreen/CurrencyScreen.jsx
+++ src/screen/CurrencyScreen/CurrencyScreen.jsx
@@ -32,56 +32,56 @@ const CurrencyScreen = () => {
     
 
     return(
-        <div class="container">
+        <div  class="currencyContainer">
            
          {isLoading ? (
              <p>Load data...</p>
          ): (
              <div>
                 <TableHeader text1="Currency" text2="We Buy" text3="Exchange Rate" text4="We Sell" />   
-                <div class="list">
+                <div class="currencyList">
                     <CurrencyTitle text="CAD"/>
-                    <text class="content-text">{CalcBuy(data.CAD)}</text>
-                    <text class="content-text">{data.CAD}</text>
-                    <text class="content-text">{CalcSell(data.CAD)}</text>
+                    <text class="currencyContentText">{CalcBuy(data.CAD)}</text>
+                    <text class="currencyContentText">{data.CAD}</text>
+                    <text class="currencyContentText">{CalcSell(data.CAD)}</text>
                 </div>
-                <div class="list">
+                <div class="currencyList">
                     <CurrencyTitle text="EUR"/>
-                    <text class="content-text">{CalcBuy(data.EUR)}</text>
-                    <text class="content-text">{data.EUR}</text>
-                    <text class="content-text">{CalcSell(data.EUR)}</text>
+                    <text class="currencyContentText">{CalcBuy(data.EUR)}</text>
+                    <text class="currencyContentText">{data.EUR}</text>
+                    <text class="currencyContentText">{CalcSell(data.EUR)}</text>
                 </div>
-                <div class="list">
+                <div class="currencyList">
                     <CurrencyTitle text="IDR"/>
-                    <text class="content-text">{CalcBuy(data.IDR)}</text>
-                    <text class="content-text">{data.IDR}</text>
-                    <text class="content-text">{CalcSell(data.IDR)}</text>
+                    <text class="currencyContentText">{CalcBuy(data.IDR)}</text>
+                    <text class="currencyContentText">{data.IDR}</text>
+                    <text class="currencyContentText">{CalcSell(data.IDR)}</text>
                 </div>
-                <div class="list">
+                <div class="currencyList">
                     <CurrencyTitle text="JPY"/>
-                    <text class="content-text">{CalcBuy(data.JPY)}</text>
-                    <text class="content-text">{data.JPY}</text>
-                    <text class="content-text">{CalcSell(data.JPY)}</text>
+                    <text class="currencyContentText">{CalcBuy(data.JPY)}</text>
+                    <text class="currencyContentText">{data.JPY}</text>
+                    <text class="currencyContentText">{CalcSell(data.JPY)}</text>
                 </div>
-                <div class="list">
+                <div class="currencyList">
                     <CurrencyTitle text="CHF"/>
-                    <text class="content-text">{CalcBuy(data.CHF)}</text>
-                    <text class="content-text">{data.CHF}</text>
-                    <text class="content-text">{CalcSell(data.CHF)}</text>
+                    <text class="currencyContentText">{CalcBuy(data.CHF)}</text>
+                    <text class="currencyContentText">{data.CHF}</text>
+                    <text class="currencyContentText">{CalcSell(data.CHF)}</text>
                 </div>
-                <div class="list">
+                <div class="currencyList">
                     <CurrencyTitle text="GBP"/>
-                    <text class="content-text">{CalcBuy(data.GBP)}</text>
-                    <text class="content-text">{data.GBP}</text>
-                    <text class="content-text">{CalcSell(data.GBP)}</text>
+                    <text class="currencyContentText">{CalcBuy(data.GBP)}</text>
+                    <text class="currencyContentText">{data.GBP}</text>
+                    <text class="currencyContentText">{CalcSell(data.GBP)}</text>
                 </div>
                 {/* {data.map((datum, i) => {
                     return <TableContent datum={datum} key={i} />
                 })} */}
-                {/* <div class="footer"> */}
+                {/* <div class="currencyFooter"> */}
                 <br />
-                <div><p class="content-text">Rates are based from 1 USD</p></div>
-                <div><p class="content-text">The application uses API from https://currencyfreaks.com</p></div>
+                <div><p class="currencyContentText">Rates are based from 1 USD</p></div>
+                <div><p class="currencyContentText">The application uses API from https://currencyfreaks.com</p></div>
             {/* </div> */}
         </div>
          )}
